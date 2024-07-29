e64fdcc52bf1c683cbf410c2b515926ffdba38f4
TS <ktmountain@naver.com>
2024-07-29 15:17:24 +0900

    fix code

---
+++ src/.ipynb_checkpoints/preprocessing-checkpoint.py
@@ -0,0 +1,104 @@
+import pickle
+import random
+import os
+import sys
+import random
+import time
+import numpy as np
+import pandas as pd
+import json
+from dataset.vocab import WordVocab
+import gc
+import datetime
+from dateutil.relativedelta import relativedelta
+import argparse
+from tqdm import tqdm
+
+import pyarrow.parquet as pq
+
+
+from os import path
+
+class Preprocessing:
+    def __init__(self, strd_ym):
+
+        self.strd_ym=strd_ym 
+        
+    def pretrain_get_data(self, data_size=20000000,min_len=32,max_len=1024, data_name='amazon'):# 64/1024
+        print("Data Loading Start with",data_name)
+        
+        root_path='data/'+self.strd_ym+'/pretrain'
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)        
+
+        data_path=root_path+"/"+data_name+"_pretrain_dataset_"+self.strd_ym+".pkl" 
+        if not path.exists(data_path):
+            amazon_seq = pd.read_table('dataset/raw/amazon_seq.txt')
+            amazon_seq=amazon_seq.drop(columns='Unnamed: 0')
+            amazon_seq.to_pickle(data_path)
+        else:
+            print('File already exists!')
+            amazon_seq=pd.read_pickle(data_path)
+
+        return amazon_seq
+
+    
+    
+    def build_voca(self,voca_raw):
+        voca = []
+        for i in voca_raw:
+            line = i.split(",")
+            voca.extend(line)
+
+        print('Voca_len: ',len(set(voca)))
+        return voca, len(set(voca))
+
+
+    def make_voca(self,data_name='amazon'):
+        root_path='pretrained_model/voca'
+        if not os.path.exists(root_path):
+            os.makedirs(root_path)   
+
+        data_path=root_path+"/"+data_name+"_voca_"+'item'+"_"+self.strd_ym+".ep" 
+
+        if not path.exists(data_path):
+            print('No file!')
+
+            columns=['type','item']
+            voca_set=[]
+            for i in columns:
+                print('voca dataset loading...')
+
+                voca = pd.read_table(f'dataset/raw/voca_{i}.txt')
+                voca = voca.drop(columns='Unnamed: 0')
+                min_freq=0        
+
+                print('Vocabularay Building...')
+                voca = WordVocab(voca, min_freq=min_freq)
+
+                output_voca_path = root_path+"/"+data_name+"_voca_"+i+"_"+self.strd_ym+".ep"
+                voca.save_vocab(output_voca_path)
+                voca = WordVocab.load_vocab(output_voca_path)
+                voca_set.append(voca)
+        else:
+            print('Vocab already exist!')
+            columns=['type','item']
+
+            voca_set=[]
+            for i in columns:
+                output_voca_path = root_path+"/"+data_name+"_voca_"+i+"_"+self.strd_ym+".ep"
+                voca = WordVocab.load_vocab(output_voca_path)
+                voca_set.append(voca)
+
+        return voca_set
+        
+
+    
+    def pretrain_loader(self,data_size,min_len,max_len,data_name):
+
+        data = self.pretrain_get_data(data_size,min_len,max_len,data_name)
+        print(data['item'].head())
+
+        voca = self.make_voca(data_name)
+    
+        return data, voca
---
+++ src/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,103 @@
+import numpy as np
+import math
+import random
+import os
+import json
+import pickle
+import torch
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, WeightedRandomSampler
+import pandas as pd
+import warnings
+
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
+
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f'{path} created')
+
+def neg_sample(item_set, item_size):
+    item = random.randint(2, item_size - 1)
+    while item in item_set:
+        item = random.randint(2, item_size - 1)
+    return item
+
+def neg_hard_sample(item_set, ture_item):
+    item = random.choice(list(item_set))
+    while item == ture_item:
+        item = random.choice(list(item_set))
+    return item
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, checkpoint_path, patience=7, verbose=False, delta=0):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+        """
+        self.checkpoint_path = checkpoint_path
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
+
+    def compare(self, score):
+        for i in range(len(score)):
+            if score[i] > self.best_score[i]+self.delta:
+                return False
+        return True
+
+    def __call__(self, score, model):
+        # score HIT@10 NDCG@10
+        if self.best_score is None:
+            self.best_score = score
+            self.score_min = np.array([0]*len(score))
+            self.save_checkpoint(score, model)
+        elif self.compare(score):
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(score, model)
+            self.counter = 0
+
+    def save_checkpoint(self, score, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation score increased.  Saving model ...')
+        torch.save(model.module.state_dict(), self.checkpoint_path)
+        self.score_min = score
+
+def get_metric(pred_list, topk=10):
+    NDCG = 0.0
+    HIT = 0.0
+    MRR = 0.0
+    # [batch] the answer's rank
+    for rank in pred_list:
+        MRR += 1.0 / (rank + 1.0)
+        if rank < topk:
+            NDCG += 1.0 / np.log2(rank + 2.0)
+            HIT += 1.0
+    return HIT /len(pred_list), NDCG /len(pred_list), MRR /len(pred_list)
